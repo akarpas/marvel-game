10104523cb6412cdf26877b06f3c491b89af670f
Andreas Karpasitis <karpasitis@outlook.com>
2018-10-25 21:44:53 +0200

    update webpack files for eslint

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -4,14 +4,14 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    main: './src/index.js'
+    main: './src/index.js',
   },
   output: {
     filename: '[name].[hash].js',
-    path: path.resolve('./dist')
+    path: path.resolve('./dist'),
   },
   performance: {
-    hints: false
+    hints: false,
   },
   module: {
     rules: [
@@ -21,38 +21,38 @@ module.exports = {
         use: [{
           loader: 'babel-loader',
           query: {
-            compact: false
-          }
-        }]
+            compact: false,
+          },
+        }],
       },
       {
         test: /\.s(a|c)ss%/,
         use: [
           {
-            loader: 'style-loader'
+            loader: 'style-loader',
           },
           {
             loader: 'css-loader',
             query: {
               modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]'
-            }
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
           },
           {
             loader: 'sass-loader',
             query: {
               modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]'
-            }
-          }
-        ]
-      }
-    ]
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html'
+      template: 'index.html',
     }),
-    new CleanWebpackPlugin(['dist'])
-  ]
-}
+    new CleanWebpackPlugin(['dist']),
+  ],
+};
--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -6,6 +6,6 @@ module.exports = merge(common, {
   devServer: {
     host: 'localhost',
     port: 3000,
-    open: true
-  }
-})
+    open: true,
+  },
+});
