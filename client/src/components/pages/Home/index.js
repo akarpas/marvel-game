6c9c759919314fc91f9d1251e1f4dd3bc489127f
Andreas Karpasitis <karpasitis@outlook.com>
2018-10-26 08:05:14 +0200

    create home page component

---
+++ client/src/components/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './index.scss';
+
+const Home = () => (
+  <div className={style.content}>
+    <div> Content </div>
+  </div>
+);
+
+export default Home;
