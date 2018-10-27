62c0ba15b50e80b301b28009c360df9cf61f0050
Andreas Karpasitis <karpasitis@outlook.com>
2018-10-27 17:36:55 +0200

    add basic tests for Home

--- client/__tests__/components/Home.test.js
+++ client/__tests__/components/Home.test.js
@@ -20,3 +20,47 @@ afterEach(() => {
 it('renders without crashing', () => {
   wrapped.render();
 });
+
+describe('header, text and footer', () => {
+  it('contains a header with a title', () => {
+    expect(wrapped.find('header').length).toEqual(1);
+    expect(wrapped.find('header').text()).toEqual('MARVEL SUPERHEROES');
+  });
+  it('contains an empty footer', () => {
+    expect(wrapped.find('footer').length).toEqual(1);
+    expect(wrapped.find('footer').text()).toEqual('');
+  });
+  it('contains a description and an instruction title', () => {
+    expect(wrapped.find('.description').length).toEqual(1);
+    expect(wrapped.find('.title').length).toEqual(1);
+    expect(wrapped.find('.description').text()).toContain('A fun memory game with your favorite Marvel superheroes!');
+    expect(wrapped.find('.title').text()).toContain('Pick your 8 Heroes:');
+  });
+});
+
+describe('main content with options', () => {
+  it('contains a section with the superheroes to select', () => {
+    expect(wrapped.find('.options').length).toEqual(1);
+  });
+  it('contains 20 buttons', () => {
+    const options = wrapped.find('.options');
+    expect(options.find('button').length).toEqual(20);
+  });
+  it('changes button class on click', () => {
+    const oneButton = wrapped.find('button').first();
+    expect(oneButton.hasClass('button')).toEqual(true);
+    oneButton.simulate('click');
+    wrapped.update();
+    expect(wrapped.find('button').first().hasClass('buttonSelected')).toEqual(true);
+  });
+});
+
+describe('action buttons - Shuffle and Play', () => {
+  it('contains a Shuffle button', () => {
+    expect(wrapped.find('#shuffle').text()).toContain('Shuffle');
+  });
+  it('contains a Play button which is disabled', () => {
+    expect(wrapped.find('#play').text()).toContain('Play');
+    expect(wrapped.find('#play').hasAttr('disabled')).toEqual(true);
+  });
+});
