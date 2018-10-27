fca96ac903676e9fe7cac6e1b3c11b5dcbf6989a
Andreas Karpasitis <karpasitis@outlook.com>
2018-10-27 17:37:19 +0200

    add a list of predefined hereos in data folder

---
+++ client/data/heroes.js
@@ -0,0 +1,24 @@
+const heroes = [
+  'Iron Man',
+  'Hulk',
+  'Captain America',
+  'Spider-man',
+  'Thanos',
+  'Ant Man',
+  'Rocket',
+  'Thor',
+  'Phoenix',
+  'Daredevil',
+  'Groot',
+  'Doctor Strange',
+  'Vision',
+  'Wolverine',
+  'War Machine',
+  'Black Panther',
+  'Deadpool',
+  'Cyclopes',
+  'Odin',
+  'Magneto',
+];
+
+export default heroes;
