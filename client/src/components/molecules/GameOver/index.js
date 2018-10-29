3a15eea2336e8112d635348b6a15a9314c7ce7b4
Andreas Karpasitis <karpasitis@outlook.com>
2018-10-29 22:10:37 +0100

    set time to 50 seconds

--- client/src/components/molecules/GameOver/index.js
+++ client/src/components/molecules/GameOver/index.js
@@ -4,7 +4,6 @@ import style from './index.scss';
 
 const GameOver = (props) => {
   const { finalScore, remainingTime, hasCompleted } = props;
-
   const calculateScore = (score, time, completed) => {
     if (completed) {
       return (score + (time * 30));
