26dbb8132d3a1bc0bfc864177c06af0898d9d0f3
Andreas Karpasitis <karpasitis@outlook.com>
2018-10-30 18:08:00 +0100

    implement logic to fetch avatars from marvel api

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const axios = require('axios');
 const shuffle = require('lodash.shuffle');
+const md5 = require('md5');
 const port = 5050;
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -31,6 +33,36 @@ app.post('/v1/api/fetch_cards', async (req, res, next) => {
     });
   }
 
+  const urls = heroes.map((hero) => {
+    const ts = Date.now() / 1000;
+    const hash = md5(ts + PRIVATE_KEY + API_KEY);
+    const baseUrl = `https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&name=`;
+    return `${baseUrl}${hero}&limit=1&apikey=${API_KEY}&hash=${hash}`;
+  });
+
+  const promises = urls.map(async (url) => {
+    try {
+      let response = await axios.get(url);
+      if (response.data.data.results.length) return { response, error: null };
+      response = await axios.get(url);
+      if (response.data.data.results.length) return { response, error: null };
+      return { response, error: 'Something went wrong with the call. No results found.' };
+    } catch (error) {
+      return { response: [], error: `Something went wrong with the call, retrying - Error: ${error}` };
+    }
+  });
+
+  const avatars = await axios.all(promises).then(axios.spread((...response) => response));
+
+  // TODO: Check for error (loop through avatars for any errors);
+
+  const avatarImages = avatars.map((avatar, index) => {
+    const { response } = avatar;
+    const { results } = response.data.data;
+    const { thumbnail } = results[0];
+    const { path, extension } = thumbnail;
+    return { image: `${path}.${extension}`, hero: heroes[index] };
+  });
   const allAvatars = shuffle(avatarImages.concat(avatarImages));
 
   res.status(200).send({
