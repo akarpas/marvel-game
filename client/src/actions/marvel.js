307429285d078caefc4a05f22fed8c808da8fd05
Andreas Karpasitis <karpasitis@outlook.com>
2018-11-01 15:39:00 +0100

    update marvel action and reducer to use server

--- client/src/actions/marvel.js
+++ client/src/actions/marvel.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import md5 from 'md5';
+import config from '../../config.json';
 import {
   GET_AVATARS,
   AVATARS_LOADING,
@@ -12,26 +12,10 @@ export const fetchAvatars = async (dispatch, heroes) => {
     payload: null,
   });
 
-  const apiKey = '15ef57ccdc882a4200f7c7165b93164a';
-  const privateKey = 'cfc373ebe901e9c090416286bc72f1d9aec6c258';
-  const urls = heroes.map((hero) => {
-    const ts = Date.now() / 1000;
-    const hash = md5(ts + privateKey + apiKey);
-    const baseUrl = `https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&name=`;
-    return `${baseUrl}${hero}&limit=1&apikey=${apiKey}&hash=${hash}`;
-  });
-  const promises = urls.map(async (url) => {
-    try {
-      let response = await axios.get(url);
-      if (response.data.data.results.length) return { response, error: null };
-      response = await axios.get(url);
-      if (response.data.data.results.length) return { response, error: null };
-      return { response, error: 'Something went wrong with the call. No results found.' };
-    } catch (error) {
-      return { response: [], error: `Something went wrong with the call, retrying - Error: ${error}` };
-    }
-  });
-  const avatars = await axios.all(promises).then(axios.spread((...response) => response));
+  const { apiUrl } = config[process.env.NODE_ENV];
+  const response = await axios.post(apiUrl, { heroes });
+  const { data } = response;
+  const { avatars } = data;
 
   return dispatch({
     type: GET_AVATARS,
