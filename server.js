9c67073cc014bd136cf117172e9ca16aaa1ac5e0
Andreas Karpasitis <karpasitis@outlook.com>
2018-11-01 13:10:29 +0100

    update package json - procfile and server files - include .env in gitignore

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const app = require('./app');
-const port = process.env.PORT || 5050;
+const app = require('./app.js');
+const port = 5050;
 
 app.listen(port, function () {
   console.log("Server is running on "+ port +" port");
