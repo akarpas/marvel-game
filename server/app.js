81332c3ac96d1164edcdaa5788ac34b5afc2f59d
Andreas Karpasitis <karpasitis@outlook.com>
2018-11-01 14:44:19 +0100

    add check if provided data is array

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.post('/v1/api/fetch_cards', async (req, res, next) => {
   }
 
   const { heroes } = req.body;
-  if (!heroes || !heroes.length) {
+  if (!heroes || !heroes.length || (typeof heroes !== 'object' && !heroes.length)) {
     return next(boom.badRequest(`Incorrect data provided! Accepts a heroes array with the names of the heroes`));
   }
 
