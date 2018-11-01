d055915bf5308c85a4872ba23dc57e9f45e27831
Andreas Karpasitis <karpasitis@outlook.com>
2018-11-01 16:53:54 +0100

    update reducer and action tests for use with the new backend

--- client/__tests__/reducers/marvel.test.js
+++ client/__tests__/reducers/marvel.test.js
@@ -1,6 +1,6 @@
 import configureStore from 'redux-mock-store';
 import marvelReducer from '../../src/reducers/marvel';
-import fetchAvatars from '../../src/actions/marvel';
+import { fetchAvatars } from '../../src/actions/marvel';
 
 import { GET_AVATARS, AVATARS_LOADING } from '../../src/actions/types';
 
@@ -34,11 +34,15 @@ describe('get avatars from marvel api', () => {
       payload,
     };
     const newState = await marvelReducer({ avatars: [] }, action);
+    const result = {
+      image: 'https://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55.jpg',
+      hero: heroes[0],
+    };
+    const results = [];
+    results.push(result);
+    results.push(result);
     expect(newState).toEqual({
-      avatars: [{
-        image: 'http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55.jpg',
-        hero: heroes[0],
-      }],
+      avatars: results,
       avatarsLoading: false,
     });
   });
