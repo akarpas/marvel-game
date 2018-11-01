2148eb927c961adb7c95faade687a49ad9091ca0
Andreas Karpasitis <karpasitis@outlook.com>
2018-11-01 14:44:56 +0100

    replace http with https

--- server/app.js
+++ server/app.js
@@ -55,7 +55,7 @@ app.post('/v1/api/fetch_cards', async (req, res, next) => {
     const { results } = response.data.data;
     const { thumbnail } = results[0];
     const { path, extension } = thumbnail;
-    return { image: `${path}.${extension}`, hero: heroes[index] };
+    return { image: `${path.replace('http', 'https')}.${extension}`, hero: heroes[index] };
   });
 
   const allAvatars = shuffle(avatarImages.concat(avatarImages));
