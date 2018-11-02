c48d7e4c595fb9e1cccfcb2514a1f987bda289da
Andreas Karpasitis <karpasitis@outlook.com>
2018-11-02 12:50:56 +0100

    add error handling to server request in case rate limit is exceeded

--- client/src/reducers/marvel.js
+++ client/src/reducers/marvel.js
@@ -2,11 +2,13 @@ import {
   GET_AVATARS,
   AVATARS_LOADING,
   CLEAR_AVATARS,
+  SET_ERROR,
 } from '../actions/types';
 
 const INITIAL_STATE = {
   avatars: [],
   avatarsLoading: false,
+  error: null,
 };
 
 const getAvatars = (state, payload) => {
@@ -18,6 +20,9 @@ const setLoading = state => ({ ...state, avatarsLoading: true });
 
 const clearAvatars = state => ({ ...state, avatars: [] });
 
+const setError = (state, error) => ({ ...state, avatars: [], avatarsLoading: false, error });
+
+
 export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
     case GET_AVATARS:
@@ -26,6 +31,8 @@ export default function (state = INITIAL_STATE, action) {
       return setLoading(state);
     case CLEAR_AVATARS:
       return clearAvatars(state);
+    case SET_ERROR:
+      return setError(state, action.payload);
     default:
       return state;
   }
